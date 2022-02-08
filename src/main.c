975d34b3fa8e2dd14ff039b5460432fa0e5d4822
Brandon D <social@brandongrows.me>
2022-02-07 22:53:42 -0600

    We has movement

--- src/main.c
+++ src/main.c
@@ -25,6 +25,12 @@ void drawEntities() {
   E2.y = 24;
   E2.tile = E2_ID;
   E2.ctrl = OAM_ENABLE;
+
+  #define SHOT OAM[17]
+  E3.x = 160;
+  E3.y = 24;
+  E3.tile = SHOT_ID;
+  E3.ctrl = OAM_ENABLE;
 }
 
 void drawItems() {
@@ -35,14 +41,55 @@ void drawItems() {
   HT.ctrl = OAM_ENABLE;
 }
 
+void handleInput() {
+  uint8_t speed = 2;
+
+  if ((~KEY_PAD & KEY_DOWN) && PC.y < 60) {
+    PC.y += speed;
+  }
+  else if ((~KEY_PAD & KEY_UP) && PC.y > 20) {
+    PC.y -= speed;
+  }
+  else if ((~KEY_PAD & KEY_LEFT) && PC.x > 20) {
+    PC.x -= speed;
+  }
+  else if ((~KEY_PAD & KEY_RIGHT) && PC.x < 92) {
+    PC.x += speed;
+  }
+  else if ((~KEY_PAD & KEY_RIGHT & KEY_UP) && PC.y > 20 && PC.x < 92) {
+    speed /= 2;
+    PC.x += speed;
+    PC.y -= speed;
+  }
+  else if ((~KEY_PAD & KEY_LEFT & KEY_UP) && PC.y > 20 && PC.x > 20) {
+    speed /= 2;
+    PC.x -= speed;
+    PC.y -= speed;
+  }
+  else if ((~KEY_PAD & KEY_RIGHT & KEY_DOWN) && PC.y < 60 && PC.x < 92) {
+    speed /= 2;
+    PC.x += speed;
+    PC.y += speed;
+  }
+  else if ((~KEY_PAD & KEY_RIGHT & KEY_UP) && PC.y > 20 && PC.x < 92) {
+    speed /= 2;
+    PC.x += speed;
+    PC.y -= speed;
+  }
+}
+
 int main()
 {
   uint8_t i;
   uint8_t tw = 16;
   uint8_t th = 12;
 
+  TMR1_OSC = 0x13;
+  TMR1_SCALE = 0x08 | 0x02 | 0x80 | 0x20;
+  TMR1_CTRL = 0x86;
+
   PRC_MODE = COPY_ENABLE|SPRITE_ENABLE|MAP_ENABLE|MAP_16X12;
-  PRC_RATE = RATE_36FPS;
+  PRC_RATE = RATE_12FPS;
 
   PRC_MAP = tiles;
   PRC_SPR = scene1;
@@ -64,5 +111,7 @@ int main()
   drawItems();
 
   for(;;) {
+    wait_vsync();
+    handleInput();
   }
 }
