48e07a20349d9666bab8fc00e3f432e6bd611fa3
Brandon D <social@brandongrows.me>
2022-02-08 18:39:32 -0600

    Initial shot improvements

--- src/main.c
+++ src/main.c
@@ -45,9 +45,12 @@ void pcShoot(d, bL, bR, bT, bB, s) {
   uint8_t yP = 0;
   uint8_t xP = 0;
   uint8_t bP = 4;
+  //uint8_t i;
+  //uint8_t sTime = 3;
 
   if (d != 0 && s >= 1) {
     SHOT.ctrl = OAM_ENABLE;
+    PRC_RATE = RATE_6FPS;
     if (d == 1 && !(SHOT.y - bP <= bT)) {
       yP = -1;
     }
@@ -60,19 +63,24 @@ void pcShoot(d, bL, bR, bT, bB, s) {
     else if (d == 4 && !(SHOT.x + bP >= bR)) {
       xP = 1;
     }
+
+    // time the shots so you can't spam them / hold too long
     s = 0;
   }
   else {
     SHOT.ctrl = !OAM_ENABLE;
+    PRC_RATE = RATE_12FPS;
   }
   SHOT.x = (PC.x + (xP * 3));
   SHOT.y = (PC.y + (yP * 3));
 }
 
-void handleInput() {
+void handleInput(p) {
   uint8_t speed = 2;
   uint8_t direction = 0;
   uint8_t shotFired = 0;
+  uint8_t shotDelay = 0;
+  uint8_t shotSpaces = 2;
 
   // bounds
   uint8_t bT = 20;
@@ -80,22 +88,33 @@ void handleInput() {
   uint8_t bL = 18;
   uint8_t bR = 94;
 
-  if (~KEY_PAD & KEY_A) {
+  if ((~KEY_PAD & KEY_A) && shotDelay == 0) {
     shotFired = 1;
+    if (p != PC.x || p != PC.y) {
+      shotSpaces--;
+      if (shotSpaces == 0) {
+        shotDelay = 1;
+        p = 0;
+      }
+    }
   }
   if ((~KEY_PAD & KEY_DOWN) && PC.y < bB) {
+    p = PC.y;
     PC.y += speed;
     direction = 2;
   }
   else if ((~KEY_PAD & KEY_UP) && PC.y > bT) {
+    p = PC.y;
     PC.y -= speed;
     direction = 1;
   }
   else if ((~KEY_PAD & KEY_LEFT) && PC.x > bL) {
+    p = PC.x;
     PC.x -= speed;
     direction = 3;
   }
   else if ((~KEY_PAD & KEY_RIGHT) && PC.x < bR) {
+    p = PC.x;
     PC.x += speed;
     direction = 4;
   }
@@ -130,6 +149,9 @@ int main()
   uint8_t tw = 16;
   uint8_t th = 12;
 
+  // Track player movement updated outside of loop
+  uint8_t prevPos = 0;
+
   TMR1_OSC = 0x13;
   TMR1_SCALE = 0x08 | 0x02 | 0x80 | 0x20;
   TMR1_CTRL = 0x86;
@@ -158,6 +180,6 @@ int main()
 
   for(;;) {
     wait_vsync();
-    handleInput();
+    handleInput(prevPos);
   }
 }
